3e7e7d88b839d3f45114b76388f0496f0cebc5d6
Lara Rodríguez <lararh393@gmail.com>
2024-07-21 23:26:18 +0200

    cambio css

--- js/script.js
+++ js/script.js
@@ -20,22 +20,43 @@ async function obtenerDatosAPI() {
     const respuesta = await fetch(
       "https://bypass-cors-beta.vercel.app/?url=https://api.preciodelaluz.org/v1/prices/all?zone=PCB"
     );
+    if (!respuesta.ok) {
+      throw new Error(`HTTP error! status: ${respuesta.status}`);
+    }
     const datos = await respuesta.json();
-    guardarDatosLocalStorage(datos);
-    
-    return datos;
+    console.log('Datos obtenidos de la API:', datos);
+    guardarDatosLocalStorage(datos.data);
+    mostrarPreciosPorHora(datos.data);
+    return datos.data;
   } catch (err) {
-    console.error(err.message);
+    console.error('Error al obtener datos de la API:', err.message);
   }
 }
-
+//Función para sacar los datos de precio por cada hora
+function mostrarPreciosPorHora(datos) {
+  console.log('Mostrando precios por hora:', datos);
+  Object.entries(datos).forEach(([hora, detalle]) => {
+    if (detalle && typeof detalle === 'object') {
+      console.log(`Hora: ${hora}, Precio: ${detalle.price}`);
+    } else {
+      console.error('Detalle inválido para hora:', hora, detalle);
+    }
+  });
+}
 // Función para guardar los datos en el LocalStorage
 function guardarDatosLocalStorage(datos) {
   const fecha = new Date().toISOString().split('T')[0]; // obtenemos la fecha actual
   localStorage.setItem('datosPrecioLuz', JSON.stringify(datos));
   localStorage.setItem('fechaDatos', fecha);
 }
-
+// Obtener datos almacenados en el LocalStorage y mostrar precios si existen
+let precioPorHora = JSON.parse(localStorage.getItem('datosPrecioLuz'));
+if (precioPorHora) {
+  console.log('Datos obtenidos del LocalStorage:', precioPorHora);
+  mostrarPreciosPorHora(precioPorHora);
+} else {
+  console.log('No hay datos en el LocalStorage');
+}
 // Función para calcular el costo de funcionamiento de los electrodomésticos
 function calcularCosto(electrodomesticos, precio) {
   const costos = {};
@@ -132,6 +153,8 @@ async function main() {
   document.getElementById("horaMayor").textContent = (`${horaMaxima}h`)
   document.getElementById("precioMayor").textContent = (`${precioMaximo} €/MWh`)
 
+  let tabla = document.getElementsById("tabla");
+  tabla.textContent = (`Hora: ${hora}, Precio: ${detalle.price}€`)
  
 }
 
